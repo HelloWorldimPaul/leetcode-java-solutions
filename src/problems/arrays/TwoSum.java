3f843729941a784d27cbedbe72c59c8ced3f4c88
HelloPaul <Paulpedrano@gmail.com>
2025-09-12 18:07:00 +0800

    Initial commit: project setup with folders

---
+++ src/problems/arrays/TwoSum.java
@@ -0,0 +1,27 @@
+package problems.arrays;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = 1; j < nums.length; j++) {
+
+                if (i == j) {
+                    continue;
+                } else if (nums[i] + nums[j] == target) {
+
+                    return new int[]{i, j};
+
+                }
+
+            }
+
+        }
+
+        return new int[]{-1, -1};
+
+    }
+
+}
